3910eaf42288f05bdce2a41d3d8b89d37fe06382
Amaury Martiny <amaury.martiny@gmail.com>
2017-08-26 21:09:09 +0200

    Add fake data in db

---
+++ rest/models/seed.js
@@ -0,0 +1,75 @@
+import sequelize from './';
+
+// Add some data in the databas
+export default function (sequelize) {
+  const models = sequelize.models;
+
+  models.User.create({
+    firstname: 'John',
+    lastname: 'Doe',
+  })
+  .then((user) => models.Project.create({
+    name: 'Migrate from REST to GraphQL',
+    UserId: user.id,
+  }))
+  .then((project) => {
+    models.Task.create({
+      description: 'Read tutorial',
+      ProjectId: project.id
+    });
+    models.Task.create({
+      description: 'Start coding',
+      ProjectId: project.id
+    });
+    return Promise.resolve(project);
+  })
+  .then((project) => models.Project.create({
+    name: 'Create a blog',
+    UserId: project.UserId,
+  }))
+  .then((project) => {
+    models.Task.create({
+      description: 'Write draft of article',
+      ProjectId: project.id
+    });
+    models.Task.create({
+      description: 'Set up blog platform',
+      ProjectId: project.id
+    });
+    return Promise.resolve();
+  })
+  .then(()=> models.User.create({
+    firstname: 'Alicia',
+    lastname: 'Smith',
+  }))
+  .then((user) => models.Project.create({
+    name: 'Email Marketing Campaign',
+    UserId: user.id,
+  }))
+  .then((project) => {
+    models.Task.create({
+      description: 'Get list of users',
+      ProjectId: project.id
+    });
+    models.Task.create({
+      description: 'Write email template',
+      ProjectId: project.id
+    });
+    return Promise.resolve(project);
+  })
+  .then((project) => models.Project.create({
+    name: 'Hire new developer',
+    UserId: project.UserId,
+  }))
+  .then((project) => {
+    models.Task.create({
+      description: 'Find candidates',
+      ProjectId: project.id
+    });
+    models.Task.create({
+      description: 'Prepare interview',
+      ProjectId: project.id
+    });
+    return Promise.resolve();
+  })
+};
